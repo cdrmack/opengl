5b321e2a52aaf14ca82e6769ee58cca2505478c6
cdrmack <git.pynvt@passmail.net>
2025-02-12 15:33:20 +0100

    add CreateTriangle function, bind VAO and VBO

--- main.c
+++ main.c
@@ -1,9 +1,38 @@
+#include <stdio.h>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <stdio.h>
 
 const GLint WIDTH = 800;
 const GLint HEIGHT = 600;
+
+GLuint VAO;
+GLuint VBO;
+GLuint shader;
+
+void CreateTriangle()
+{
+    // triangle, 3 vertices
+    GLfloat vertices[] = {-1.0f, -1.0f, 0.0f, 1.0f, -1.0f,
+                          0.0f,  0.0f,  1.0f, 0.0f};
+
+    // what data is
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // actual data
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // 3 floats for each vertex (x,y,z)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    // unbind stuff
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
 
 int main()
 {
