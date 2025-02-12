b39300b8bd5a0d00ea04752cf92eeca7d68d9ad4
cdrmack <git.pynvt@passmail.net>
2025-02-12 16:08:16 +0100

    compile and attach shaders, draw triangle

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -17,7 +18,7 @@ layout(location = 0) in vec3 pos;\n\
 \n\
 void main()\n\
 {\n\
-    gl_Position = vec4(pos, 1.0f);\n\
+    gl_Position = vec4(0.5 * pos, 1.0f);\n\
 }\n\
 ";
 
@@ -31,6 +32,71 @@ void main()\n\
     color = vec4(0.0f, 1.0f, 0.0f, 1.0f);\n\
 }\n\
 ";
+
+void AddShader(GLuint program, const char *shaderCode, GLenum shaderType)
+{
+    GLuint shader = glCreateShader(shaderType);
+
+    const GLchar *code[1]; // = shaderCode;
+    code[0] = shaderCode;
+
+    GLint length[1];
+    length[0] = strlen(shaderCode);
+
+    glShaderSource(shader, 1, code, length);
+    glCompileShader(shader);
+
+    GLint result = 0;
+    GLchar errorBuffer[1024] = {0};
+
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+    if (!result)
+    {
+        glGetShaderInfoLog(shader, sizeof(errorBuffer), NULL, errorBuffer);
+        printf("compiling shader %d failed with error: %s\n", shaderType,
+               errorBuffer);
+        return; // TODO, handle error
+    }
+
+    glAttachShader(program, shader);
+}
+
+void CompileShaders()
+{
+    shader = glCreateProgram();
+
+    if (!shader)
+    {
+        printf("glCreateProgram failed\n");
+        return; // TODO, handle error
+    }
+
+    AddShader(shader, vertexShader, GL_VERTEX_SHADER);
+    AddShader(shader, fragmentShader, GL_FRAGMENT_SHADER);
+
+    GLint result = 0;
+    GLchar errorBuffer[1024] = {0};
+
+    // create executables on the GPU
+    glLinkProgram(shader);
+
+    glGetProgramiv(shader, GL_LINK_STATUS, &result);
+    if (!result)
+    {
+        glGetProgramInfoLog(shader, sizeof(errorBuffer), NULL, errorBuffer);
+        printf("linking program failed with error: %s\n", errorBuffer);
+        return; // TODO, handle error
+    }
+
+    glValidateProgram(shader);
+    glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
+    if (!result)
+    {
+        glGetProgramInfoLog(shader, sizeof(errorBuffer), NULL, errorBuffer);
+        printf("validating program failed with error: %s\n", errorBuffer);
+        return; // TODO, handle error
+    }
+}
 
 void CreateTriangle()
 {
@@ -104,6 +170,9 @@ int main()
     // setup viewport size
     glViewport(0, 0, bufferWidth, bufferHeight);
 
+    CreateTriangle();
+    CompileShaders();
+
     while (!glfwWindowShouldClose(mainWindow))
     {
         glfwPollEvents();
@@ -111,6 +180,14 @@ int main()
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         // each pixel has more data than color (for example depth)
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader);
+        glBindVertexArray(VAO);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glBindVertexArray(0);
+        glUseProgram(0);
 
         glfwSwapBuffers(mainWindow);
     }
